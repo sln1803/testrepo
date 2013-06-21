ad5e6c19d77e6a89ba40810ffe65395eba17b7ec
sln1803 <shane.clarke@outlook.com>
2013-06-21 15:13:44 +0100

    [nobug] edit17

--- org.test.bundle/src/org/test/bundle/Activator.java
+++ org.test.bundle/src/org/test/bundle/Activator.java
@@ -8,7 +8,7 @@ import org.osgi.framework.BundleContext;
  * The activator class controls the plug-in life cycle
  */
 public class Activator extends AbstractUIPlugin {
-    //edit16
+    //edit17
 	
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.test.bundle"; //$NON-NLS-1$
